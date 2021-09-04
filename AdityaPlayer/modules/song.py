04dc408e72f09eef8b7ad78a808ca36dab569826
COBRAXDOP <85988755+COBRAXDOP@users.noreply.github.com>
2021-09-04 15:52:06 +0530

    Create song.py

--- AdityaPlayer/modules/song.py
+++ AdityaPlayer/modules/song.py
@@ -76,7 +76,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Ƭɦɩs Søɳʛ ɩs Ʋƥɭøɑɗɘɗ Ɓƴ [Ʌɗɩtyɑ](https://t.me/AdityaHalder) ...**"
+        rep = "**🎵 Ƭɦɩs Søɳʛ ɩs Ʋƥɭøɑɗɘɗ Ɓƴ [αℓเƶα](https://t.me/AlizaProBotbot) ...**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
